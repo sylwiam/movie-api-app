c0ed934d6b6e4cb6e3b9ea177b08b2d47abec1e0
Sylwia Decowski <sylwiadecowski@gmail.com>
2016-08-07 17:04:50 -0400

    organize  url file

--- metadata_api/urls.py
+++ metadata_api/urls.py
@@ -4,12 +4,15 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^load-data$', views.loadData, name='loadData'),
+    
     url(r'^titles$', views.getAllTitles, name='get-all-titles'),
     url(r'^titles/latest$', views.getLatestTitles, name='get-latest-titles'),
     url(r'^titles/(?P<primaryKey>[0-9]+)/$', views.getTitleDetails, name='get-title-detail'),
 
-    # url enfpoint for migrating data between tables
+    # url endpoint for loading initial data into Temp table
+    url(r'^load-data$', views.loadData, name='load-data'),
+    
+    # url endpoint for migrating data between tables
     url(r'^migrate-records$', views.migrateRecords, name='migrate-records'),
 
 ]
